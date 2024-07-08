393fa04a7adbf3db4e3d3b5d2a3ac9f54d0cf538
sprjr <103283283+sprjr@users.noreply.github.com>
2024-07-08 13:58:23 -0600

    Create gaming-desktop.nix

---
+++ nixos/gaming-desktop.nix
@@ -0,0 +1,253 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./modules/shell/starship.nix
+      ./modules/virtualisation/containers/syncthing.nix
+      ./modules/gaming/sunshine.nix
+     #./modules/monitoring/graylog.nix
+      ./modules/monitoring/node-exporter.nix
+     #./modules/network/netbird-server.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-f9b0a070-8711-4b4a-84ac-a70044729daf".device = "/dev/disk/by-uuid/f9b0a070-8711-4b4a-84ac-a70044729daf";
+  networking.hostName = "seanix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Disable NetworkManager-wait-online.service
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true; # GUI management
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable Wake on Lan
+  networking.interfaces.enp34s0.wakeOnLan.enable = true;
+
+  # Firewall Port allowances
+  networking.firewall.allowedTCPPortRanges = [
+    # KDE Connect
+    { from = 1714; to = 1764; }
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    # KDE Connect
+    { from = 1714; to = 1764; }
+  ];
+
+  # Enable Docker
+  virtualisation.docker = {
+    enable = true;
+  };
+
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.patrick = {
+    isNormalUser = true;
+    description = "Patrick";
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
+    packages = with pkgs; [
+      kdePackages.kate
+      (wineWowPackages.full.override {
+        wineRelease = "staging";
+        mingwSupport = true;
+      })
+      winetricks
+    ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "Hack" "DroidSansMono" "JetBrainsMono" ]; })
+  ];
+
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  environment.interactiveShellInit = ''
+    alias ls='lsd -l'
+    alias cat='bat'
+    alias df='duf'
+    alias gomuks='docker run -e TERM=xterm -it --rm heywoodlh/gomuks'
+    alias kubectl='k3s kubectl'
+    alias compose2nix-start='nix shell github:aksiksi/compose2nix'
+    alias yt-dl='nix shell -p yt-dlp'
+    alias weather='curl -s v2.wttr.in/saratoga+springs+utah'
+    alias bf='du -aBm / 2>/dev/null | sort -nr | head -n 20'
+  '';
+
+
+  # Enable Steam
+  programs.steam.enable = true;
+
+  # Flatpaks
+  services.flatpak.enable = true;
+
+  # Tailscale
+  services.tailscale.enable = true;
+
+  # Mullvad
+  services.mullvad-vpn.enable = true;
+  networking.iproute2.enable = true;
+
+  # Wireguard
+  networking.wireguard.enable = true;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-compose
+    fanctl
+    git
+    ollama
+    pciutils
+    pipewire
+    thermald
+    wireguard-tools
+    wget
+
+    # User environment
+    activitywatch
+    alacritty
+    bat # pretty cat
+    bitwarden
+    bitwarden-cli
+    discord
+    duf
+    file
+    fzf
+    gamescope
+    glow # in-text markdown reader
+    htop # pretty top
+    kdePackages.kdeconnect-kde
+    ladybird
+    lima
+    lsd # pretty ls
+    mdp # fullscreen markdown reader
+    mullvad-browser
+    neovim
+    obsidian
+    signal-desktop
+    starship
+    thunderbird
+    tldr
+    tree
+    vim
+    vimPlugins.nvchad
+
+    ### Net tools ###
+    tailscale
+    inetutils
+    mtr
+    mullvad-vpn
+    lshw
+    nmap
+    remmina
+    syncthing
+    wireguard-tools
+    wireshark
+
+    # Gaming
+    heroic
+    prismlauncher
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
