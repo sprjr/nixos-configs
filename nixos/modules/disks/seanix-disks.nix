96be4acbf76d4baea24d2222556426c4db02e39d
sprjr <patrick@rawlinson.ws>
2024-08-22 12:24:25 -0600

    updated disk config

--- nixos/modules/disks/seanix-disks.nix
+++ nixos/modules/disks/seanix-disks.nix
@@ -3,7 +3,7 @@
 {
   # 1TB Samsung 860 m.2
   fileSystems."/run/media/patrick/6655e2e3-ce9e-42eb-8043-6a553ced1d76" = {
-    device = "/dev/disk/6655e2e3-ce9e-42eb-8043-6a553ced1d76";
+    device = "/dev/disk/by-uuid/b6655e2e3-ce9e-42eb-8043-6a553ced1d76";
     fsType = "ext4";
     options = [
       "nofail" # prevent system from failing if the drive doesn't mount
