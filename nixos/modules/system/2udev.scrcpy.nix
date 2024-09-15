ab7a423503a68fb9ecd0d235fd35aade019ba726
sprjr <patrick@rawlinson.ws>
2024-09-15 08:38:41 -0600

    updated scrcpy config, no longer erroring out

--- nixos/modules/system/2udev.scrcpy.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, ...}:
-
-{
-  services.udev.extraRules = ''
-      SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="/home/patrick/Documents/Scripts/bash/personal/system/scrcpy-trigger.sh"
-  '';
-}
--- nixos/modules/system/udev-scrcpy.nix
+++ nixos/modules/system/udev-scrcpy.nix
@@ -1,13 +1,9 @@
 { config, lib, pkgs, ...}:
 
-let
-  scrcpy-rules = pkgs.writeTextFile {
-    name = "81-scrcpy-trigger.rules";
-    text = ''
-      SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="/home/patrick/Documents/Scripts/bash/personal/system/scrcpy-trigger.sh"
-    '';
-    destination = "/etc/udev/rules.d/81-scrcpy-trigger.rules";
-  };
-in {
-  services.udev.packages = [ scrcpy-rules ];
+{
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="${pkgs.writeShellScript "scrcpy-trigger" ''
+        ${pkgs.scrcpy}/bin/scrcpy
+    ''}"
+  '';
 }
