22a5fa421cd67fdbf58f681dcfc36dcb6e938f95
sprjr <patrick@rawlinson.ws>
2024-09-14 21:26:02 -0600

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -81,8 +81,8 @@
           ./nixos/modules/virtualisation/containers/syncthing.nix
           ./nixos/modules/gaming/sunshine.nix
           ./nixos/modules/monitoring/node-exporter.nix
-          ./nixos/modules/disks/seanix-disks.nix # not automounting yet 
-          ./nixos/modules/system/2udev-scrcpy.nix
+          ./nixos/modules/disks/seanix-disks.nix # not automounting yet
+          ./nixos/modules/system/udev-scrcpy.nix
           {
           # Additional configuration goes here
           }
