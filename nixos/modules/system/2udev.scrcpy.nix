40888fbdd6a6f566109c3213e39e2043fba15015
sprjr <patrick@rawlinson.ws>
2024-09-14 21:34:15 -0600

    updated scrcpy testing

--- nixos/modules/system/2udev.scrcpy.nix
+++ nixos/modules/system/2udev.scrcpy.nix
@@ -1,12 +1,7 @@
-{ config, pkgs, ...}:
+{ config, lib, pkgs, ...}:
 
 {
-  services.udev = {
-    enable = true;
-    extraRules = [
-    ''
+  services.udev.extraRules = ''
       SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="/home/patrick/Documents/Scripts/bash/personal/system/scrcpy-trigger.sh"
-    '';
-    ];
-  };
+  '';
 }
