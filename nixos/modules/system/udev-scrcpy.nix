fd49461da630030a3879e1f06138024dba37d2b7
sprjr <patrick@rawlinson.ws>
2024-09-16 10:34:38 -0600

    further updated scrcpy rules to include a systemd solution instead of a bash script

--- nixos/modules/system/udev-scrcpy.nix
+++ nixos/modules/system/udev-scrcpy.nix
@@ -1,9 +1,22 @@
 { config, lib, pkgs, ...}:
 
+let
+  scrcpy-trigger = pkgs.writeShellScript "scrcpy-trigger" ''
+    export DISPLAY=:0
+    ${pkgs.scrcpy}/bin/scrcpy
+  '';
+in
 {
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="${pkgs.writeShellScript "scrcpy-trigger" ''
-        ${pkgs.scrcpy}/bin/scrcpy
-    ''}"
-  '';
+    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="${pkgs.systemd}/bin/systemctl start scrcpy-trigger.service"
+    '';
+
+  systemd.services.scrcpy-trigger = {
+    path = [ "${pkgs.scrcpy}/bin/scrcpy" ];
+    description = "scrcpy service to auto-run on Pixel mount";
+    serviceConfig = {
+      ExecStart = scrcpy-trigger;
+      User = "patrick";
+    };
+  };
 }
