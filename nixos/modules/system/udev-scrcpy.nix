954c84098281d51c70bb6de5f9c8d8c4458fadbd
sprjr <patrick@rawlinson.ws>
2024-09-17 09:36:24 -0600

    finalized scrcpy config (thank you heywoodlh)

--- nixos/modules/system/udev-scrcpy.nix
+++ nixos/modules/system/udev-scrcpy.nix
@@ -2,18 +2,20 @@
 
 let
   scrcpy-trigger = pkgs.writeShellScript "scrcpy-trigger" ''
-    export DISPLAY=:0
     ${pkgs.scrcpy}/bin/scrcpy
   '';
 in
 {
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", ACTION=="add", RUN+="${pkgs.systemd}/bin/systemctl start scrcpy-trigger.service"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee7", TAG+="systemd", ENV{SYSTEMD_WANTS}="scrcpy-trigger.service", ACTION=="add", RUN+="${pkgs.systemd}/bin/systemctl start scrcpy-trigger.service"
     '';
 
   systemd.services.scrcpy-trigger = {
     path = [ "${pkgs.scrcpy}/bin/scrcpy" ];
     description = "scrcpy Service";
+    environment = {
+      XDG_RUNTIME_DIR = "/run/user/1000";
+    };
     serviceConfig = {
       ExecStart = scrcpy-trigger;
       User = "patrick";
