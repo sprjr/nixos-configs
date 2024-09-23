8db7475c4c7e2ab715bd23496924ad2067665ef8
sprjr <patrick@rawlinson.ws>
2024-09-23 09:35:09 -0600

    updated disks

--- flake.nix
+++ flake.nix
@@ -44,6 +44,7 @@
           ./nixos/hardware-configuration/trixos.nix
           ./nixos/gaming-laptop.nix
           ./nixos/modules/nvidia.nix
+          ./nixos/modules/disks/hetzner-box-cifs.nix
           {
             networking.hostName = "trixos";
             hardware.nvidia.prime = {
