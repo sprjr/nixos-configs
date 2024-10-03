001e2b63c44172e5b5fffe16a1d421b9ebe845d3
sprjr <patrick@rawlinson.ws>
2024-10-02 22:53:10 -0600

    updated zellij config

--- home/home.nix
+++ home/home.nix
@@ -112,30 +112,35 @@
     enable = true;
   };
 
-  programs.zellij.settings = {
-    theme = "nord";
-    themes.nord = {
-      fg = "#D8DEE9";
-      bg = "#2E3440";
-      black = "#3B4252";
-      red = "#BF616A";
-      green = "#A3BE8C";
-      yellow = "#EBCB8B";
-      blue = "#81A1C1";
-      magenta = "#B48EAD";
-      cyan = "#88C0D0";
-      white = "#E5E9F0";
-      orange = "#D08770";
+  programs.zellij = {
+    settings = {
+      theme = "nord";
+      themes.nord = {
+        fg = "#D8DEE9";
+        bg = "#2E3440";
+        black = "#3B4252";
+        red = "#BF616A";
+        green = "#A3BE8C";
+        yellow = "#EBCB8B";
+        blue = "#81A1C1";
+        magenta = "#B48EAD";
+        cyan = "#88C0D0";
+        white = "#E5E9F0";
+        orange = "#D08770";
+      };
     };
-    layout = "default";
-      layouts.default = {
-        layout = {
+    layouts = {
+      default = ''
+        layout {
 	  pane
 	  pane split_direction="vertical" {
-	    pane command="btop"
-	  };
-	};
-      };
+	    pane
+	    pane split_direction="vertical" {
+	      pane command="btop"
+	    }
+	  }
+	}
+      '';
     };
   };
 
