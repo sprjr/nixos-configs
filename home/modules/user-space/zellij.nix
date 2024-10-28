8b0019f21aadc578aa6d682ad19b551a2c18887c
sprjr <patrick@rawlinson.ws>
2024-10-28 17:07:51 -0600

    changed approach again

--- home/modules/user-space/zellij.nix
+++ home/modules/user-space/zellij.nix
@@ -1,48 +1,45 @@
-{ config, pkgs, home-manager, lib, ... }:
+{ config, pkgs, home-manager, ... }:
 
 let
-  zellijConfig = pkgs.writeTextFile {
-    name = "config.kdl";
-    text = ''
-      layout {
-        default layout {
+  zellijConfigContent = ''
+    layout {
+      default layout {
+        pane {
+          split_direction "Horizontal"
+        }
+        pane {
+          split_direction "Vertical"
           pane {
-            split_direction "Horizontal"
           }
           pane {
-            split_direction "Vertical"
-            pane {
-            }
-            pane {
-              command "btop"
-            }
+            command "btop"
           }
         }
       }
-      theme "nord"
-      themes {
-        nord {
-          bg "#2E3440"
-          black "#3B4252"
-          blue "#81A1C1"
-          cyan "#88C0D0"
-          fg "#D8DEE9"
-          green "#A3BE8C"
-          magenta "#B48EAD"
-          orange "#D08770"
-          red "#BF616A"
-          white "#E5E9F0"
-          yellow "#EBCB8B"
-        }
+    }
+    theme "nord"
+    themes {
+      nord {
+        bg "#2E3440"
+        black "#3B4252"
+        blue "#81A1C1"
+        cyan "#88C0D0"
+        fg "#D8DEE9"
+        green "#A3BE8C"
+        magenta "#B48EAD"
+        orange "#D08770"
+        red "#BF616A"
+        white "#E5E9F0"
+        yellow "#EBCB8B"
       }
-    '';
-  };
+    }
+  '';
 in
 
 {
-  programs.zellij = {
-    enable = true;
-    settings.config = zellijConfig;
-  };
+  programs.zellij.enable = true;
+
+  # Write config.kdl directly to ~/.config/zellij/config.kdl
+  home.file.".config/zellij/config.kdl".text = zellijConfigContent;
 }
 
