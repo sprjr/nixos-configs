aae80da0619d4861463a289a2cc10bd0e81fa17c
sprjr <patrick@rawlinson.ws>
2024-10-28 22:33:48 -0600

    updated zellij setup

---
+++ home/modules/user-space/zellij-layout.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, home-manager, ... }:
+
+let
+  zellijConfigContent = ''
+    theme "nord"
+    themes {
+      nord {
+        bg "#2E3440"
+        black "#3B4252"
+        blue "#81A1C1"
+        cyan "#88C0D0"
+        fg "#D8DEE9"
+        green "#A3BE8C"
+        magenta "#B48EAD"
+        orange "#D08770"
+        red "#BF616A"
+        white "#E5E9F0"
+        yellow "#EBCB8B"
+      }
+    }
+  '';
+in
+
+{
+  programs.zellij.enable = true;
+
+  # Write config.kdl directly to ~/.config/zellij/config.kdl
+  home.file.".config/zellij/config.kdl".text = zellijConfigContent;
+}
