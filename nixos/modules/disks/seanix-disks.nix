386187aa08753f7947b4676e4f02d840512ca2e3
sprjr <patrick@rawlinson.ws>
2024-11-13 07:38:58 -0700

    changed disk mount location from /run/...

--- nixos/modules/disks/seanix-disks.nix
+++ nixos/modules/disks/seanix-disks.nix
@@ -2,7 +2,7 @@
 
 {
   # 1TB Samsung 860 m.2
-  fileSystems."/run/media/patrick/6655e2e3-ce9e-42eb-8043-6a553ced1d76" = {
+  fileSystems."/mnt/media/6655e2e3-ce9e-42eb-8043-6a553ced1d76" = {
     device = "/dev/disk/by-uuid/b6655e2e3-ce9e-42eb-8043-6a553ced1d76";
     fsType = "ext4";
     options = [
