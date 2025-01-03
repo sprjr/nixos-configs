bdb843e28640d97d4430adb5e0e4f3ac531928fd
sprjr <patrick@rawlinson.ws>
2025-01-03 17:34:32 +0900

    adding home-manager config for ghostty

--- home/home.nix
+++ home/home.nix
@@ -170,5 +170,23 @@
     pinentry_mac
   ];
 
+  # Ghostty configuration
+  home.file.".config/ghostty/config" = {
+    text = ''
+      font-family = ""
+      font-family = "JetBrains Mono"
+      theme = nord
+      background-opacity = 0.7
+      background-blur-radius = 20
+      window-decoration = false
+      # https://github.com/ghostty-org/ghostty/pull/3742
+      # quick-terminal-size = 80%
+
+      # quake mode; on MacOS give Ghostty accessibility permissions
+      keybind = global:ctrl+grave_accent=toggle_quick_terminal
+      quick-terminal-animation-duration = 0.1
+    '';
+  };
+
   home.stateVersion = "24.05";
 }
