4fb7869e65c4e144dfad42c287478c901dd10ed0
sprjr <patrick@rawlinson.ws>
2025-01-14 09:38:44 +0900

    updated zsh to change term to screen-256color on startup

--- home/modules/tools/shell.nix
+++ home/modules/tools/shell.nix
@@ -25,6 +25,31 @@
 	yt-dl = "nix-shell -p yt-dlp";
       };
     };
+    zsh = {
+      enable = true;
+      initExtra = ''
+        # Run fish if it's not already running
+        if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
+        then
+          exec fish -l
+        fi
+        export TERM=screen-256color
+      '';
+      # Terminal startup tasks
+      # Aliases
+      shellAliases = {
+	bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
+ 	cat = "bat";
+	compose2nix-start = "nix shell github:aksiksi/compose2nix";
+	df = "duf";
+	dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
+	dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
+	kubectl = "k3s kubectl";
+        ls = "lsd -l";
+	weather = "curl -s v2.wttr.in/saratoga+springs+utah";
+	yt-dl = "nix-shell -p yt-dlp";
+      };
+    };
     fish = {
       enable = true;
       interactiveShellInit = ''
@@ -42,29 +67,6 @@
         ls = "lsd -l";
         weather = "curl -s v2.wttr.in/saratoga+springs+utah";
         yt-dl = "nix-shell -p yt-dlp";
-      };
-    };
-    zsh = {
-      enable = true;
-      initExtra = ''
-        if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
-        then
-          exec fish -l
-        fi
-      '';
-      # Terminal startup tasks
-      # Aliases
-      shellAliases = {
-	bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
- 	cat = "bat";
-	compose2nix-start = "nix shell github:aksiksi/compose2nix";
-	df = "duf";
-	dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
-	dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
-	kubectl = "k3s kubectl";
-        ls = "lsd -l";
-	weather = "curl -s v2.wttr.in/saratoga+springs+utah";
-	yt-dl = "nix-shell -p yt-dlp";
       };
     };
   };
