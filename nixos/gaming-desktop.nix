a28eead3f949abbeccbf106905e53837036198d0
sprjr <patrick@rawlinson.ws>
2025-03-05 09:17:40 -0700

    moved os-prober

--- nixos/gaming-desktop.nix
+++ nixos/gaming-desktop.nix
@@ -15,7 +15,7 @@ in {
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+# boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
