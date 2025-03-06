7316e056b890c3fb904056a85e0096700d21d209
sprjr <patrick@rawlinson.ws>
2025-03-06 14:07:37 -0700

    playing with opacity

--- home/modules/tools/ghostty.nix
+++ home/modules/tools/ghostty.nix
@@ -7,7 +7,7 @@
       font-family = "JetBrains Mono"
       theme = nord
       bold-is-bright = true
-      background-opacity = 0.7
+      background-opacity = 1
       background-blur = 20
       term = screen-256color
       cursor-style = bar
