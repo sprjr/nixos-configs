06b98d1756855120311df1fe226d31b542961a18
sprjr <patrick@rawlinson.ws>
2025-03-20 11:35:19 -0600

    backing out of gnome-extension changes until I can figure it out later

--- nixos/modules/desktop-environment/gnome.nix
+++ nixos/modules/desktop-environment/gnome.nix
@@ -13,26 +13,26 @@
   programs.dconf.enable = true;
 
   # Setup user-specific extensions
-  home-manager.users.patrick = {
-    dconf = {
-      enable = true;
-      settings = {
-        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-       "org/gnome/shell" = {
-         disable-user-extensions = false;
-         enabled-extensions = with pkgs.gnomeExtensions; [
-           blur-my-shell@aunetx
-           caffeine@patapon.info
-           gsconnect@ahdyholmes.github.io
-           just-perfection-desktop@just-perfection
-           paperwm@paperwm.github.com
-           search-light@icedman.github.com
-           space-bar@luchrioh
-         ];
-       };
-     };
-    };
-  };
+#  home-manager.users.patrick = {
+#    dconf = {
+#      enable = true;
+#      settings = {
+#        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+#        "org/gnome/shell" = {
+#          disable-user-extensions = false;
+#          enabled-extensions = with pkgs.gnomeExtensions; [
+#            blur-my-shell@aunetx
+#            caffeine@patapon.info
+#            gsconnect@ahdyholmes.github.io
+#            just-perfection-desktop@just-perfection
+#            paperwm@paperwm.github.com
+#            search-light@icedman.github.com
+#            space-bar@luchrioh
+#          ];
+#        };
+#      };
+#    };
+#  };
 
   # Enable gnome-settings-daemon udev rules
   services.udev.packages = with pkgs; [ gnome-settings-daemon ];
