1bf1037b86fe01f52fc4b54671874edfddb7da38
sprjr <patrick@rawlinson.ws>
2025-04-08 14:11:35 -0600

    adding wireless

--- nixos/seanix.nix
+++ nixos/seanix.nix
@@ -21,7 +21,7 @@ in {
   boot.initrd.luks.devices."luks-f9b0a070-8711-4b4a-84ac-a70044729daf".device = "/dev/disk/by-uuid/f9b0a070-8711-4b4a-84ac-a70044729daf";
   networking.hostName = "seanix"; # Define your hostname.
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Disable NetworkManager-wait-online.service
   systemd.services.NetworkManager-wait-online.enable = false;
