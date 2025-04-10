c07ccf55b1b58103e2e5cba7fab6fd2627709046
sprjr <patrick@rawlinson.ws>
2025-04-09 23:34:02 -0600

    removed bad import

--- nixos/modules/system/sops.nix
+++ nixos/modules/system/sops.nix
@@ -1,8 +1,6 @@
 { pkgs, inputs, config, ... }:
 
 {
-  imports = [ <sops-nix/modules/sops> ];
-
   sops.defaultSopsFile = ../../../sops-nix/sops.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/patrick/.config/sops/age/key.txt";
