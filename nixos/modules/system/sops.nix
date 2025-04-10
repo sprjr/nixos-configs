2ce080fe1b36abc73c00b543b947fb904f51d6e3
sprjr <patrick@rawlinson.ws>
2025-04-10 00:08:06 -0600

    fixing typo

--- nixos/modules/system/sops.nix
+++ nixos/modules/system/sops.nix
@@ -3,7 +3,7 @@
 {
   sops.defaultSopsFile = ../../../sops-nix/sops.yaml;
   sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile = "/home/patrick/.config/sops/age/key.txt";
+  sops.age.keyFile = "/home/patrick/.config/sops/age/keys.txt";
 
   sops.secrets.kubernetes-homelab-node-key = { };
 }
