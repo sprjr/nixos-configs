f2d777d78d4f3150bf280f0b82cfc9e139bc7cd6
sprjr <patrick@rawlinson.ws>
2025-04-10 15:01:53 -0600

    testing if injecting the secret works

--- nixos/modules/virtualisation/k3s-node.nix
+++ nixos/modules/virtualisation/k3s-node.nix
@@ -1,18 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  sops.secrets.kubernetes-homelab-node-key = { };
-  sops.templates."../../../../sops-nix/sops.yaml" = {
-    owner = "patrick";
-    content = ''
-      password = "${config.sops.placeholder.kubernetes-homelab-node-key}"
-    '';
-  };
-
   services.k3s = {
     enable = true;
     role = "agent";
-    token = "${config.sops.placeholder.kubernetes-homelab-node-key}";
+    token = { token = config.sops.secrets."kubernetes-homelab-node-key"; };
     serverAddr = "https://100.67.20.13:6443";
   };
 }
