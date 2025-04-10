ea12e562b36cdb48dc7e8d4706be74cb1cc49840
sprjr <patrick@rawlinson.ws>
2025-04-10 15:01:42 -0600

    updated sops.nix configuration

--- nixos/modules/system/sops.nix
+++ nixos/modules/system/sops.nix
@@ -1,9 +1,13 @@
 { pkgs, inputs, config, ... }:
 
 {
-  sops.defaultSopsFile = ../../../sops-nix/sops.yaml;
-  sops.defaultSopsFormat = "yaml";
-  sops.age.keyFile = "/home/patrick/.config/sops/age/keys.txt";
+  sops = {
+    defaultSopsFile = ../../../sops-nix/sops.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "/home/patrick/.config/sops/age/keys.txt";
 
-  sops.secrets.kubernetes-homelab-node-key = { };
+    secrets = {
+      "kubernetes-homelab-node-key".owner = "patrick";
+    };
+  };
 }
