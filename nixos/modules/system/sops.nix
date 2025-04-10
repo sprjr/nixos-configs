c41f76c2b8005551fa7fa6866573e8fefb1a3d59
sprjr <patrick@rawlinson.ws>
2025-04-09 23:29:44 -0600

    testing sops and k3s

--- nixos/modules/system/sops.nix
+++ nixos/modules/system/sops.nix
@@ -1,6 +1,8 @@
 { pkgs, inputs, config, ... }:
 
 {
+  imports = [ <sops-nix/modules/sops> ];
+
   sops.defaultSopsFile = ../../../sops-nix/sops.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/patrick/.config/sops/age/key.txt";
