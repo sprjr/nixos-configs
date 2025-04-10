c681d28d68230fa8919efdc766b7bfb383dc33eb
sprjr <patrick@rawlinson.ws>
2025-04-09 23:43:14 -0600

    adding secret

--- nixos/modules/system/sops.nix
+++ nixos/modules/system/sops.nix
@@ -9,4 +9,6 @@
   sops.defaultSopsFile = ../../../sops-nix/sops.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/patrick/.config/sops/age/key.txt";
+
+  sops.secrets.kubernetes-homelab-node-key = { };
 }
