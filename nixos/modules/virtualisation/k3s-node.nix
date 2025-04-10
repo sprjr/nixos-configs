cbb6963c3bd6ec2c467a4aea3bbfa556d2f34b37
sprjr <patrick@rawlinson.ws>
2025-04-10 16:14:20 -0600

    changing format around injecting sops-nix secret

--- nixos/modules/virtualisation/k3s-node.nix
+++ nixos/modules/virtualisation/k3s-node.nix
@@ -14,7 +14,7 @@
   services.k3s = {
     enable = true;
     role = "agent";
-    token = { token = config.sops.secrets."kubernetes-homelab-node-key"; };
+    token = config.sops.secrets."kubernetes-homelab-node-key";
     serverAddr = "https://100.67.20.13:6443";
   };
 }
