ee9b312913dd0d86a846f05de480cd63731f4f1c
sprjr <patrick@rawlinson.ws>
2025-04-11 16:07:15 -0600

    changing name from home.nix for clarity

---
+++ home/linux-home.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, home-manager, ... }:
+
+{
+  # Modules
+  imports = [
+    ./linux/desktop_environments/gnome.nix
+    ./modules/general/packages.nix
+    ./modules/tools/cli-visualizer.nix
+    ./modules/tools/ghostty.nix
+    ./modules/tools/neovim.nix
+    ./modules/user-space/bat.nix
+    ./modules/user-space/shell.nix
+    ./modules/user-space/zellij/zellij-layout-darwin.nix
+    ./modules/user-space/zellij/zellij-config.nix
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "sprjr";
+    userEmail = "patrick@rawlinson.ws";
+  };
+
+  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/patrick" else "/home/patrick";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  home.stateVersion = "24.05";
+}
