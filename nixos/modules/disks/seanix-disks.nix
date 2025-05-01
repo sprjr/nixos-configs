9ef9b811e9bc424c385f1264c0db31af68cb31e8
sprjr <patrick@rawlinson.ws>
2025-05-01 12:35:10 -0600

    changing approach from filesystems to systemd.mounts

--- nixos/modules/disks/seanix-disks.nix
+++ nixos/modules/disks/seanix-disks.nix
@@ -1,19 +1,20 @@
 { pkgs, lib, ... }:
 
 {
-  # 1TB Samsung 860 m.2
-  fileSystems."/mnt/media/drive0" = {
-    device = "/dev/disk/by-uuid/6655e2e3-ce9e-42eb-8043-6a553ced1d76";
-    fsType = "ext4";
-    options = [
-      "nofail" # prevent system from failing if the drive doesn't mount
-      "noatime"
-      "defaults"
-    ];
-  };
+  systemd.mounts = [{
+    what = "/dev/disk/by-uuid/6655e2e3-ce9e-42eb-8043-6a553ced1d76";
+    where = "/mnt/media/data";
+    type = "ext4";
+    options = "defaults,nofail,noatime";
+    wantedBy = [ "multi-user.target" ];
+    enable = true;
+  }];
 
-  # Ensure mount point directory exists:
+  # Ensure the mount point directory exists
   systemd.tmpfiles.rules = [
-    "d /mnt/media/drive0 0755 root root - -"
+    "d /mnt/media/data 0755 root root - -"
   ];
+
+  # Also add this to ensure the mount works
+  boot.supportedFilesystems = [ "ext4" ];
 }
