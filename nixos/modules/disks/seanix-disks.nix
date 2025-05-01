5d375de459fa24fb5d5ff5bc60d043d2a57c07a0
sprjr <patrick@rawlinson.ws>
2025-05-01 12:01:43 -0600

    fixing disk uuid typo

--- nixos/modules/disks/seanix-disks.nix
+++ nixos/modules/disks/seanix-disks.nix
@@ -3,15 +3,15 @@
 {
   # 1TB Samsung 860 m.2
   fileSystems."/mnt/media/drive-0" = {
-    device = "/dev/disk/by-uuid/b6655e2e3-ce9e-42eb-8043-6a553ced1d76";
+    device = "/dev/disk/by-uuid/6655e2e3-ce9e-42eb-8043-6a553ced1d76";
     fsType = "ext4";
     options = [
       "nofail" # prevent system from failing if the drive doesn't mount
       "noatime"
       "defaults"
       "user"
-      "x-systemd.automount"
-      "x.systemd.idle-timeout=1min"
+#     "x-systemd.automount"
+#     "x.systemd.idle-timeout=1min"
     ];
   };
 }
