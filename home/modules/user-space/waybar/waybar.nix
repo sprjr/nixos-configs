0be14869eb7e9cee5cac6baf29474092ec01b74d
sprjr <patrick@rawlinson.ws>
2025-05-30 11:35:04 -0600

    testing new waybar config

---
+++ home/modules/user-space/waybar/waybar.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, home-manager, lib, ... }:
+
+{
+  programs.waybar = {
+    enable = true;
+    settings = [
+      {
+        mainBar = {
+	  layer = "top";
+	  position = "top";
+	  height = 30;
+	  output = [
+	    "eDP-1"
+	    #"HDMI-A-1"
+	  ];
+	  modules-left = [
+	    "sway/workspaces"
+	    "sway/mode"
+	    "wlr/taskbar"
+	  ];
+	  modules-center = [
+	    "sway/window"
+	    "custom/hello-from-waybar"
+	  ];
+	  modules-right = [
+	    "mpd"
+	    "temperature"
+	  ];
+
+	  "sway/workspaces" = {
+	    disable-scroll = true;
+	    all-outputs = true;
+	  };
+	  "custom/hello-from-waybar" = {
+	    format = "hello {}";
+	    max-length = 40;
+	    interval = "once";
+	    exec = pkgs.writeShellScript "hello-from-waybar" ''
+	      echo "from within waybar"
+	    '';
+	  };
+	};
+      }
+    ];
+  };
+}
