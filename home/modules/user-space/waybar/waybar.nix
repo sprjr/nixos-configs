8e5657467bd179531adc502d07c065eaff103a71
sprjr <patrick@rawlinson.ws>
2025-05-31 00:41:03 -0600

    updated styling

--- home/modules/user-space/waybar/waybar.nix
+++ home/modules/user-space/waybar/waybar.nix
@@ -47,11 +47,42 @@
 	font-size: 13px;
 	color: #ffffff;
       }
+
       window#waybar {
-        background-color: rgba(40, 44, 52, 0.9);
+        background-color: #2E3440;
+	border-bottom: 1px solid #4C566A;
       }
+
       #clock {
+        padding: 0 10px;
+	color: #ECEFF4;
+	background-color: #3B4252;
+	border-radius: 6px;
+	margin: 2px;
         font-weight: bold;
+      }
+
+      #battery.charging {
+        color: #A3BE8C;
+      }
+
+      #battery.warning {
+        color: #EBCB8B;
+      }
+
+      #battery.critical {
+        color: #BF616A;
+      }
+
+      #workspaces button.focused {
+        background-color: #5E81AC;
+	color: #ECEFF4;
+      }
+
+      #workspaces button {
+        padding: 0 6px;
+	color: #D8DEE9;
+	background-color: #2E3440;
       }
     '';
   };
