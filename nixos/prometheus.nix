c3c571a0a43c8e24cd9b06ecaba226e316c4e618
sprjr <patrick@rawlinson.ws>
2025-05-31 00:03:57 -0600

    adding

--- nixos/prometheus.nix
+++ nixos/prometheus.nix
@@ -5,6 +5,7 @@ let
   pkgs-stable = nixpkgs-stable.legacyPackages.${system};
 in {
   imports = [
+    <home-manager/nixos>
     home-manager.nixosModules.home-manager
     ./modules/system/sops.nix
   ];
