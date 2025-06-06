130b105618ad2ae966829e10844d7bc0ce20a8de
sprjr <patrick@rawlinson.ws>
2025-06-06 12:33:51 -0600

    added systemd-service for garage

---
+++ nixos/modules/homelab/storage/garage-systemd-service.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  systemd.services.garage = {
+    description = "S3 Garage Object Store";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.garage}/bin/garage server -c /etc/garage.toml";
+      Restart = "on-failure";
+      User = "garage";
+      Group = "garage";
+      WorkingDirectory = "/var/lib/garage";
+    };
+  };
+
+  users.users.garage = {
+    isSystemUser = true;
+    group = "garage";
+    home = "/var/lib/garage";
+  };
+
+  users.groups.garage = {};
+
+  # Open needed ports
+  networking.firewall.allowedTCPPorts = [ 3900 3901 3902 3903 3904 ];
+}
