8ccece1a0aba9b1b2b6fc79460d3ed8222c1b6b6
sprjr <patrick@rawlinson.ws>
2025-06-19 22:37:35 -0600

    updating sound options

--- nixos/seanix.nix
+++ nixos/seanix.nix
@@ -96,6 +96,9 @@ in {
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable sound state saving
+  sound.enable = true;
+
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
