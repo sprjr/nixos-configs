ae62bfea8279d7017cdeab38b3be1a85db190d04
sprjr <patrick@rawlinson.ws>
2025-07-15 12:14:33 -0600

    adding `allow broken`

--- nixos/seanix.nix
+++ nixos/seanix.nix
@@ -32,6 +32,9 @@ in {
   # Disable NetworkManager-wait-online.service
   systemd.services.NetworkManager-wait-online.enable = false;
 
+  # TEMPORARY Allow Broken
+  nixpkgs.config.allowBroken = true;
+
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
