abfdb79c91376bc6fbce2bbd5d4d5a8cd2cea629
sprjr <patrick@rawlinson.ws>
2025-08-22 21:22:31 -0600

    adding resolved to handle dns

--- nixos/prometheus.nix
+++ nixos/prometheus.nix
@@ -61,6 +61,16 @@ in {
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Enable resolved to handle DNS issue after sleeping
+  networking.nameservers = [" 1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    dnsovertls = "true";
+  };
+
   # Firewall Port allowances
   networking.firewall.allowedTCPPortRanges = [
     # KDE Connect
