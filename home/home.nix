9eda3b3dfa2049316ed93f51b0c68ecec9f10fb9
sprjr <patrick@rawlinson.ws>
2025-08-26 23:48:17 -0600

    removing waybar

--- home/home.nix
+++ home/home.nix
@@ -11,7 +11,7 @@
 #   ./modules/user-space/hyprland/hyprland.nix
 #   ./modules/user-space/hyprland/hyprlock.nix
 #   ./modules/user-space/hyprland/hypridle.nix
-    ./modules/user-space/waybar/waybar.nix
+#   ./modules/user-space/waybar/waybar.nix
     ./modules/user-space/zellij/zellij-layout-darwin.nix
     ./modules/user-space/zellij/zellij-config.nix
   ];
