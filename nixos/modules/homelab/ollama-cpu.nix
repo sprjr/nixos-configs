1751491e452d497cf636ce0371b545d54a7d2c32
sprjr <patrick@rawlinson.ws>
2025-09-03 12:03:34 -0600

    updating host information

--- nixos/modules/homelab/ollama-cpu.nix
+++ nixos/modules/homelab/ollama-cpu.nix
@@ -27,8 +27,8 @@
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";
       SCARF_NO_ANALYTICS = "True";
-      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+      OLLAMA_API_BASE_URL = "http://0.0.0.0:11434/api";
+      OLLAMA_BASE_URL = "http://0.0.0.0:11434";
     };
   };
 
