85485707a6831f38397c615528272481c3cdb2f9
sprjr <patrick@rawlinson.ws>
2025-10-17 09:01:16 -0600

    updating zellij config

--- home/linux-home.nix
+++ home/linux-home.nix
@@ -10,7 +10,7 @@
     ./modules/user-space/bat.nix
 #   ./modules/user-space/btop.nix
     ./modules/user-space/shell.nix
-    ./modules/user-space/zellij/zellij-layout-darwin.nix
+    ./modules/user-space/zellij/zellij-layout.nix
     ./modules/user-space/zellij/zellij-config.nix
   ];
 
