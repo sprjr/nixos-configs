f53a74ce5ad840a897119fba6303ee4ba1fd2d12
sprjr <patrick@rawlinson.ws>
2025-10-23 15:47:54 -0600

    adding fprintd

---
+++ nixos/modules/system/fprintd.nix
@@ -0,0 +1,15 @@
+{ configs, pkgs, ... }:
+
+{
+  environment.systemPackages = [
+    pkgs.fprintd
+  ];
+
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-vfs0090;
+    };
+  };
+}
