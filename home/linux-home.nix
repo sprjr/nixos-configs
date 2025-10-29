e31eb4c7b3f8fd1d635286fdc0071763e96b0012
sprjr <patrick@rawlinson.ws>
2025-10-28 21:09:05 -0600

    adding ghostty config to linux-home.nix

--- home/linux-home.nix
+++ home/linux-home.nix
@@ -6,6 +6,7 @@
     ./linux/desktop_environments/gnome.nix
     ./modules/general/packages.nix
     ./modules/tools/alacritty.nix
+    ./modules/tools/ghostty.nix
     ./modules/tools/neovim.nix
     ./modules/user-space/bat.nix
 #   ./modules/user-space/btop.nix
