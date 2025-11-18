674ed67f2ec8e555b0523c3f07d4f042fbd22bbb
sprjr <patrick@rawlinson.ws>
2025-11-18 10:20:46 -0700

    starting new config for kid usage

---
+++ nixos/seagull.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, lib, home-manager, nixpkgs-stable, ... }:
+
+let
+  system = pkgs.system;
+  pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+in {
+  imports = [
+    home-manager.nixosModules.home-manager
+  ];
+
+  # Home Manager
+  home-manager = {
+    useGlobalPkgs = true;
+    users.patrick = {
+      imports = [
+        ../home/linux-home.nix
+      ];
+    };
+  };
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # General Networking Options
+  networking.hostName = "whale"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant !! cannot use with networking.networkmanager.enable = true
+
+  # Disable NetworkManager-wait-online.service
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Fonts
+  fonts.packages = [
+    pkgs.nerd-fonts.hack
+    pkgs.nerd-fonts.droid-sans-mono
+    pkgs.nerd-fonts.jetbrains-mono
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Tailscale
+  services.tailscale.enable = true;
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    # Kid stuff
+    firefox
+    gcompris
+
+    # Dad tools
+    btop
+    file
+    fzf
+    git
+    mdp # fullscreen markdown reader
+    sops
+    vim
+    vimPlugins.nvchad
+    wget
+    zsh
+
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Garbage collection
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "delete-older-than 14d";
+  };
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
