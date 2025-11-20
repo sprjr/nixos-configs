c53fd33db084f5fce6925d874ebefdba43a2e46a
sprjr <patrick@rawlinson.ws>
2025-11-20 13:37:59 -0700

    cleaning up spacing

--- home/modules/user-space/shell.nix
+++ home/modules/user-space/shell.nix
@@ -8,9 +8,9 @@
       enable = true;
       settings = {
         aws = {
-	  format = "[$symbol$profile]($style)";
-	  disabled = true;
-	};
+          format = "[$symbol$profile]($style)";
+          disabled = true;
+        };
       };
     };
     # Bash/Starship
@@ -31,22 +31,22 @@
       '';
       # Aliases
       shellAliases = {
-	bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
- 	cat = "bat";
-	comfin = "paplay /home/patrick/Documents/Obs-Studio/halo_game-over.mp3";
-	compose2nix-start = "nix shell github:aksiksi/compose2nix";
-	df = "duf";
-	dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
-	dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
-	hmrb = "nix run nixpkgs#home-manager -- build -f ./home/home.nix";
-	kubectl = "sudo k3s kubectl";
+        bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
+        cat = "bat";
+        comfin = "paplay /home/patrick/Documents/Obs-Studio/halo_game-over.mp3";
+        compose2nix-start = "nix shell github:aksiksi/compose2nix";
+        df = "duf";
+        dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
+        dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
+        hmrb = "nix run nixpkgs#home-manager -- build -f ./home/home.nix";
+        kubectl = "sudo k3s kubectl";
         ls = "lsd -l";
-	weather = "curl -s v2.wttr.in/saratoga+springs+utah";
-	yt-dl = "nix-shell -p yt-dlp";
+        weather = "curl -s v2.wttr.in/saratoga+springs+utah";
+        yt-dl = "nix-shell -p yt-dlp";
 
         # For fun/dumb aliases
-	anaconda = "ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#666600,0.5:#00ff00,1:#003300";
-	rainbow = "ternimal length=20 thickness=70,15,0,1,0 padding=10 radius=5 gradient=0.03:#ffff00,0.15:#0000ff,0.3:#ff0000,0.5:#00ff00";
+        anaconda = "ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#666600,0.5:#00ff00,1:#003300";
+        rainbow = "ternimal length=20 thickness=70,15,0,1,0 padding=10 radius=5 gradient=0.03:#ffff00,0.15:#0000ff,0.3:#ff0000,0.5:#00ff00";
         swarm = "ternimal length=200 thickness=0,4,19,0,0";
       };
     };
@@ -62,19 +62,19 @@
         # Define cht function (cheat.sh helper)
       '';
       shellAliases = {
-	bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
- 	cat = "bat";
-	compose2nix-start = "nix shell github:aksiksi/compose2nix";
-	df = "duf";
-	dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
-	dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
-	hmrb = "nix run nixpkgs#home-manager -- build -f ./home/home.nix";
+        bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
+        cat = "bat";
+        compose2nix-start = "nix shell github:aksiksi/compose2nix";
+        df = "duf";
+        dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
+        dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
+        hmrb = "nix run nixpkgs#home-manager -- build -f ./home/home.nix";
         kubectl = "k3s kubectl";
         ls = "lsd -l";
-	tree = "tree -C";
+        tree = "tree -C";
         weather = "curl -s v2.wttr.in/saratoga+springs+utah";
-	yt-dl = "nix-shell -p yt-dlp";
-	cdn = "cd ~/.nixos-configuration/nixos-configs/";
+        yt-dl = "nix-shell -p yt-dlp";
+        cdn = "cd ~/.nixos-configuration/nixos-configs/";
 
         # For fun/dumb aliases
         anaconda = "ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#666600,0.5:#00ff00,1:#003300";
@@ -87,9 +87,9 @@
       interactiveShellInit = ''
         set fish_greeting # disable greeting
         # direnv
-	eval (${pkgs.direnv}/bin/direnv hook fish)
-	if status is-interactive
-	  eval (zellij setup --generate-auto-start fish | string collect)
+        eval (${pkgs.direnv}/bin/direnv hook fish)
+        if status is-interactive
+          eval (zellij setup --generate-auto-start fish | string collect)
         end
         function cht
           if test (count $argv) -eq 0
@@ -107,22 +107,22 @@
       # Aliases
       shellAliases = {
         bf = "du -aBm / 2>/dev/null | sort -nr | head -n 20";
- 	cat = "bat";
+        cat = "bat";
         compose2nix-start = "nix shell github:aksiksi/compose2nix";
-	comfin = "paplay /home/patrick/Documents/Obs-Studio/halo_game-over.mp3";
+        comfin = "paplay /home/patrick/Documents/Obs-Studio/halo_game-over.mp3";
         df = "duf";
         dfl = "du -aBm ./ 2>/dev/null | sort -nr | head -n 20";
         dockername = "docker inspect --format='{{.Name}}' $(sudo docker ps -aq --no-trunc)";
         docres = "docker restart $(docker ps -q)";
-	hmrb = "nix run nixpkgs#home-manager -- build -f ./home/home.nix";
+        hmrb = "nix run nixpkgs#home-manager -- build -f ./home/home.nix";
         kubectl = "k3s kubectl";
         ls = "lsd -l";
         weather = "curl -s v2.wttr.in/saratoga+springs+utah";
         yt-dl = "nix-shell -p yt-dlp";
 
         # For fun/dumb aliases
-	anaconda = "ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#666600,0.5:#00ff00,1:#003300";
-	rainbow = "ternimal length=20 thickness=70,15,0,1,0 padding=10 radius=5 gradient=0.03:#ffff00,0.15:#0000ff,0.3:#ff0000,0.5:#00ff00";
+        anaconda = "ternimal length=100 thickness=1,4,1,0,0 radius=6,12 gradient=0:#666600,0.5:#00ff00,1:#003300";
+        rainbow = "ternimal length=20 thickness=70,15,0,1,0 padding=10 radius=5 gradient=0.03:#ffff00,0.15:#0000ff,0.3:#ff0000,0.5:#00ff00";
         swarm = "ternimal length=200 thickness=0,4,19,0,0";
       };
     };
