9a058a1c7b84a6041ecd3c678ae5f553be12afd9
sprjr <patrick@rawlinson.ws>
2025-11-21 13:41:32 -0700

    updating for home-manager rebuild

--- home/modules/user-space/zellij/zellij-layout-desktop.nix
+++ home/modules/user-space/zellij/zellij-layout-desktop.nix
@@ -1,7 +1,7 @@
 { config, pkgs, home-manager, ... }:
 
 let
-  zellijLayoutsContent = ''
+  zellijLayoutsContentDesktop = ''
     layout {
       tab name="main" {
         pane borderless=true {
@@ -40,7 +40,6 @@ in
 
   # Write config.kdl directly to ~/.config/zellij/config.kdl
   home.file.".config/zellij/layouts/default.kdl" = {
-    text = zellijLayoutsContent;
-    force = true;
+    text = zellijLayoutsContentDesktop;
   };
 }
