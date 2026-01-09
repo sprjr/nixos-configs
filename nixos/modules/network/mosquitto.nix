e3bbd66a397a745a8c704f758517c0ecb41a384e
sprjr <patrick@rawlinson.ws>
2026-01-09 12:58:18 -0700

    updating mosquitto config

--- nixos/modules/network/mosquitto.nix
+++ nixos/modules/network/mosquitto.nix
@@ -5,5 +5,17 @@
     enable = true;
     package = pkgs.mosquitto;
     persistence = true;
+    listeners = [
+      {
+        acl = [ "pattern readwrite #" ];
+	omitPasswordAuth = true; # change later TODO
+	settings.allow_anonymous = true;
+      }
+    ];
+  };
+
+  networks.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 1883 ];
   };
 }
