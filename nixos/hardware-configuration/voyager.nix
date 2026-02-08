321ecca6d3df10a3b5bf18cf5c1497bc5c19df8b
sprjr <patrick@rawlinson.ws>
2026-02-07 23:20:35 -0700

    fixing typo

--- nixos/hardware-configuration/voyager.nix
+++ nixos/hardware-configuration/voyager.nix
@@ -24,5 +24,5 @@
   swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enabledRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
