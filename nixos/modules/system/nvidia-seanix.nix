7d2b3869c1a7a455fef7e4cfe13f8e88a9cf19d8
sprjr <patrick@rawlinson.ws>
2026-03-23 16:24:35 -0600

    initial commit for testing nvidia hardware. May need to change depending on hardware compatibility

---
+++ nixos/modules/system/nvidia-seanix.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Reference:
+  # https://nixos.wiki/wiki/Nvidia
+
+  # Also, an input (nvidia-patch) has been added to the flake.nix inputs
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load Nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  nixpkgs.overlays = [inputs.nvidia-patch.overlays.default]; # github.com/icewind1991/nvidia-patch-nixos
+
+  hardware.nvidia = {
+    # Modesetting is required
+    modesetting.enable = true;
+    powerManagement = {
+      enable = false;
+      finegrained = false;
+    };
+    open = true;
+    nvidiaSettings = true; # Nvidia Settings menu. Active with `nvidia-settings`.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+}
