a31fdcf0e43e5b1be0582e0ccd52db33d0f97fcc
sprjr <patrick@rawlinson.ws>
2026-03-24 15:01:24 -0600

    modifying to use heywoodlh configuration as a base to test with

--- nixos/modules/system/nvidia-seanix.nix
+++ nixos/modules/system/nvidia-seanix.nix
@@ -1,29 +1,41 @@
-{ config, lib, pkgs, nvidia-patch, nixpkgs, ... }:
+{ pkgs, config, nvidia-patch, nixpkgs, ... }:
 
-{
-  # Reference:
-  # https://nixos.wiki/wiki/Nvidia
-
-  # Also, an input (nvidia-patch) has been added to the flake.nix inputs
-
-  # Enable OpenGL
+let
+  system = pkgs.stdenv.hostPlatform.system;
+  pkgs-nvidia = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+    overlays = [ nvidia-patch.overlays.default ];
+  };
+  # possible versions for the package here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix
+  targetPkg = config.boot.kernelPackages.nvidiaPackages.stable;
+  pkgAfterFbc = if builtins.hasAttr targetPkg.version pkgs-nvidia.nvidia-patch-list.fbc
+                then pkgs-nvidia.nvidia-patch.patch-fbc targetPkg
+                else targetPkg;
+  finalPkg   = if builtins.hasAttr targetPkg.version pkgs-nvidia.nvidia-patch-list.nvenc
+                then pkgs-nvidia.nvidia-patch.patch-nvenc pkgAfterFbc
+                else pkgAfterFbc;
+in {
   hardware.graphics = {
     enable = true;
   };
-
-  # Load Nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
-  nixpkgs.overlays = [inputs.nvidia-patch.overlays.default]; # github.com/icewind1991/nvidia-patch-nixos
-
+  boot.kernelPackages = pkgs-nvidia.linuxKernel.packages.linux_zen;
   hardware.nvidia = {
-    # Modesetting is required
     modesetting.enable = true;
-    powerManagement = {
-      enable = false;
-      finegrained = false;
-    };
-    open = true;
-    nvidiaSettings = true; # Nvidia Settings menu. Active with `nvidia-settings`.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = finalPkg;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+    nvidiaSettings = true;
   };
+
+  services.xserver.deviceSection = ''
+    Option         "TripleBuffer" "on"
+    Option         "Coolbits" "28"
+  '';
+
+  services.xserver.screenSection = ''
+    Option         "metamodes" "nvidia-auto-select +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+    Option         "AllowIndirectGLXProtocol" "off"
+  '';
 }
