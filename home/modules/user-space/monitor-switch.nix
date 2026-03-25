533ea6c361757f377123fb23d402c0431384bc84
sprjr <patrick@rawlinson.ws>
2026-03-25 17:01:31 -0600

    rotation value as an integer is bad -- switched to string (none,left,right,inverted) to fix

--- home/modules/user-space/monitor-switch.nix
+++ home/modules/user-space/monitor-switch.nix
@@ -7,9 +7,9 @@
       text = ''
         #!/usr/bin/env bash
         kscreen-doctor \
-          output.DP-1.enable output.DP-1.mode.2 output.DP-1.position.3191,1080 output.DP-1.scale.1 output.DP-1.rotation.1 \
-          output.DP-2.enable output.DP-2.mode.16 output.DP-2.position.1920,1080 output.DP-2.scale.1.7 output.DP-2.rotation.2 \
-          output.DP-3.enable output.DP-3.mode.45 output.DP-3.position.5751,1080 output.DP-3.scale.1.6 output.DP-3.rotation.1 \
+          output.DP-1.enable output.DP-1.mode.2 output.DP-1.position.3191,1080 output.DP-1.scale.1 \
+          output.DP-2.enable output.DP-2.mode.16 output.DP-2.position.1920,1080 output.DP-2.scale.1.7 output.DP-2.rotation.left \
+          output.DP-3.enable output.DP-3.mode.45 output.DP-3.position.5751,1080 output.DP-3.scale.1.6 \
           output.HDMI-A-1.disable
       '';
     };
@@ -19,7 +19,7 @@
       text = ''
         #!/usr/bin/env bash
         kscreen-doctor \
-          output.HDMI-A-1.enable output.HDMI-A-1.mode.74 output.HDMI-A-1.position.0,0 output.HDMI-A-1.scale.1 output.HDMI-A-1.rotation.1 \
+          output.HDMI-A-1.enable output.HDMI-A-1.mode.74 output.HDMI-A-1.position.0,0 output.HDMI-A-1.scale.1 \
           output.DP-1.disable \
           output.DP-2.disable \
           output.DP-3.disable
