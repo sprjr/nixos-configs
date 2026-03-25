54a19f02594d666ee8526f212198983b864b672f
sprjr <patrick@rawlinson.ws>
2026-03-24 22:56:24 -0600

    testing removing kvm-amd from live config

--- nixos/hardware-configuration/seanix.nix
+++ nixos/hardware-configuration/seanix.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+# boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
