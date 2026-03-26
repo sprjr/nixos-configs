b596df2bdc18b9c4679301f58d0117a220d43ae9
sprjr <patrick@rawlinson.ws>
2026-03-26 16:51:36 -0600

    changing from prior format to resolution strings and avoiding static mode IDs as apparently those can change

--- home/modules/user-space/monitor-switch.nix
+++ home/modules/user-space/monitor-switch.nix
@@ -6,12 +6,10 @@
       executable = true;
       text = ''
         #!/usr/bin/env bash
-        # Values determined by running `kscreen-doctor -o`.
-
-	kscreen-doctor \
-          output.DP-1.enable output.DP-1.mode.2 output.DP-1.position.3191,1080 output.DP-1.scale.1 \
-          output.DP-2.enable output.DP-2.mode.16 output.DP-2.position.1920,1080 output.DP-2.scale.1.7 output.DP-2.rotation.left \
-          output.DP-3.enable output.DP-3.mode.45 output.DP-3.position.5751,1080 output.DP-3.scale.1.6 \
+        kscreen-doctor \
+          output.DP-1.enable output.DP-1.mode.2560x1440@165 output.DP-1.position.3191,1080 output.DP-1.scale.1 \
+          output.DP-2.enable output.DP-2.mode.3840x2160@60 output.DP-2.position.1920,1080 output.DP-2.scale.1.7 output.DP-2.rotation.left \
+          output.DP-3.enable output.DP-3.mode.3840x2160@60 output.DP-3.position.5751,1080 output.DP-3.scale.1.6 \
           output.HDMI-A-1.disable
       '';
     };
@@ -21,7 +19,7 @@
       text = ''
         #!/usr/bin/env bash
         kscreen-doctor \
-          output.HDMI-A-1.enable output.HDMI-A-1.mode.74 output.HDMI-A-1.position.0,0 output.HDMI-A-1.scale.1 \
+          output.HDMI-A-1.enable output.HDMI-A-1.mode.1920x1080@60 output.HDMI-A-1.position.0,0 output.HDMI-A-1.scale.1 \
           output.DP-1.disable \
           output.DP-2.disable \
           output.DP-3.disable
