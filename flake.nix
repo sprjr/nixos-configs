89dbd832c58657ca4b37048d6eb8a0ed58c0d874
sprjr <patrick@rawlinson.ws>
2026-03-27 14:03:13 -0600

    reverting last change

--- flake.nix
+++ flake.nix
@@ -147,7 +147,7 @@
       };
       seanix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
+        specialArgs = inputs;
         modules = [
           comin.nixosModules.comin
           sops-nix.nixosModules.sops
