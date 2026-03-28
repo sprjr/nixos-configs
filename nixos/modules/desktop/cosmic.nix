6e65b9ddfff38e5ae283d8bd191e76d1fa33f269
sprjr <patrick@rawlinson.ws>
2026-03-27 21:49:31 -0600

    better approach to handling the hash in this attempt

--- nixos/modules/desktop/cosmic.nix
+++ nixos/modules/desktop/cosmic.nix
@@ -2,7 +2,18 @@
 
 {
   imports = [ nixos-cosmic.nixosModules.default ];
-  nixpkgs.overlays = [ nixos-cosmic.overlays.default ];
+
+  nixpkgs.overlays = [
+    nixos-cosmic.overlays.default
+    (final: prev: {
+      cosmic-edit = prev.cosmic-edit.overrideAttrs (old: {
+        src = old.src.override {
+          hash = "sha256-GN1Zts+v3ARcrkN+ZkMUSGNOAlIhXSYWRtWAyqUfUrY=";
+        };
+      });
+    })
+  ];
+
   nix.settings = {
     substituters = [ "https://cosmic.cachix.org" ];
     trusted-public-keys = [ "cosmic.cachix.org-1:Dya6IxT5r5k6SJOsGKFGMEMQDcWlBoAN1JgaoL/hMKE=" ];
