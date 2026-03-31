a7a325bbe9b7852d8d1d6aae4c03c5ab9bd3fc62
sprjr <patrick@rawlinson.ws>
2026-03-31 11:56:05 -0600

    removing vim mappings -- just gonna learn hx syntax

--- home/modules/tools/helix/config.nix
+++ home/modules/tools/helix/config.nix
@@ -25,27 +25,6 @@
       [editor.lsp]
       auto-signature-help = true
       display-inlay-hints = false
-
-      [keys.normal]
-      # x/X: delete char (Vim) vs select line (Helix default)
-      x     = "delete_char_forward"
-      X     = "delete_char_backward"
-
-      # s: delete char + insert (Vim) vs select_regex (Helix default)
-      s     = "change_selection"
-
-      # D/C/Y: operate to end of line, matching Vim behavior
-      D     = ["extend_to_line_end", "delete_selection"]
-      C     = ["extend_to_line_end", "change_selection"]
-      Y     = ["extend_to_line_bounds", "yank", "collapse_selection"]
-
-      # G: end of file (Vim) vs goto_line (Helix default)
-      G     = "goto_file_end"
-
-      # Preserve displaced Helix commands on Alt bindings
-      "A-x" = "select_line_below"
-      "A-s" = "select_regex"
-      "A-g" = "goto_line"
     '';
   };
 }
