8143f0135dd607334df5d1c9242271c59ae49a12
sprjr <patrick@rawlinson.ws>
2026-05-11 16:24:29 -0600

    updating ollama environment variables

--- nixos/modules/homelab/ollama-nvidia.nix
+++ nixos/modules/homelab/ollama-nvidia.nix
@@ -13,8 +13,13 @@
       "qwen3.5:35b"
       "qwen3.5:9b"
     ];
+    environmentVariables = {
+      OLLAMA_FLASH_ATTENTION = "1";
+      OLLAMA_NUM_GPU = "999";
+      OLLAMA_GPU_OVERHEAD = "0";
+      CUDA_VISIBLE_DEVICES = "0";
+    };
   };
-
   systemd.services.ollama.serviceConfig = {
     Environment = [ "OLLAMA_HOST=0.0.0.0:11434" ];
   };
