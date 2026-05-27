36569ac4efe2b157c996b691b4b4cabbf849f29d
sprjr <patrick@rawlinson.ws>
2026-05-26 18:18:18 -0600

    removing unneeded ? from wallpaper input url

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
     };
     # Custom stuff
     dark-wallpaper-laptop = {
-      url = "https://github.com/dharmx/walls/blob/main/nord/a_cartoon_of_a_spider_man.jpg?raw=true";
+      url = "https://github.com/dharmx/walls/blob/main/nord/a_cartoon_of_a_spider_man.jpg";
       flake = false;
     };
   };
