f7fe0617770ce43bbfdd2b4fbce3fcf886d01862
sprjr <patrick@rawlinson.ws>
2026-06-03 20:33:21 -0600

    updating broken hash

--- nixos/modules/desktop/kde-glass.nix
+++ nixos/modules/desktop/kde-glass.nix
@@ -1,4 +1,10 @@
-{ config, lib, pkgs, home-manager, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  home-manager,
+  ...
+}:
 
 let
   cfg = config.services.desktopManager.plasma6;
@@ -7,7 +13,7 @@ let
     owner = "4v3ngR";
     repo = "glassOS";
     rev = "e7c689adda7a8fc4d76d641a55b0f6b8521965c3";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-2FkpfJz+Mx6P5hKj6rPFUVaSQeXKzrUkeSgmAr8er5A=";
   };
 
   kwin-effects-glass = pkgs.stdenv.mkDerivation {
@@ -84,34 +90,33 @@ lib.mkIf cfg.enable {
       "aurorae/themes/glassOS".source = "${glassOS-src}/share/aurorae/themes/glassOS";
     };
 
-    home.activation.kdeGlassConfig =
-      home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key Brightness 0.9
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key Contrast 1.1
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key Saturation 1.0
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key BlurStrength 3
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key TintColor '"#00000000"'
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key GlowColor '"#1080D0FF"'
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Effect-blurplus --key EdgeLighting true
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group Plugins --key glassEnabled true
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kdeglobals --group KDE --key widgetStyle Glass
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kdeglobals --group General --key ColorScheme GlassDark
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group "org.kde.kdecoration2" --key library org.kde.glass
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file kwinrc --group "org.kde.kdecoration2" --key theme '""'
-        run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
-          --file konsolerc --group "Desktop Entry" --key DefaultProfile dark.profile
-      '';
+    home.activation.kdeGlassConfig = home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key Brightness 0.9
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key Contrast 1.1
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key Saturation 1.0
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key BlurStrength 3
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key TintColor '"#00000000"'
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key GlowColor '"#1080D0FF"'
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Effect-blurplus --key EdgeLighting true
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group Plugins --key glassEnabled true
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kdeglobals --group KDE --key widgetStyle Glass
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kdeglobals --group General --key ColorScheme GlassDark
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group "org.kde.kdecoration2" --key library org.kde.glass
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file kwinrc --group "org.kde.kdecoration2" --key theme '""'
+      run ${lib.getExe' pkgs.kdePackages.kconfig "kwriteconfig6"} \
+        --file konsolerc --group "Desktop Entry" --key DefaultProfile dark.profile
+    '';
   };
 }
