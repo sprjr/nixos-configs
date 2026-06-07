26ad0c0431d74668030ef4b3be2f21edba312962
sprjr <patrick@rawlinson.ws>
2026-06-06 20:19:46 -0600

    adding needed config to make standalone

--- nixos/modules/desktop/cosmic.nix
+++ nixos/modules/desktop/cosmic.nix
@@ -1,4 +1,4 @@
-{ nixos-cosmic, ... }:
+{ nixos-cosmic, pkgs, ... }:
 
 {
   imports = [ nixos-cosmic.nixosModules.default ];
@@ -21,4 +21,31 @@
     substituters = [ "https://cosmic.cachix.org" ];
     trusted-public-keys = [ "cosmic.cachix.org-1:Dya6IxT5r5k6SJOsGKFGMEMQDcWlBoAN1JgaoL/hMKE=" ];
   };
+
+  services = {
+    desktopManager.cosmic.enable = true;
+    displayManager.cosmic-greeter.enable = true;
+    # Disable Orca screen reader that can mistakenly start enabled by default on Cosmic
+    orca.enable = false;
+    libinput.enable = true;
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+  };
+
+  security.rtkit.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+  # JetBrains Mono Nerd Font is used by the Cosmic user-space config
+  fonts.packages = [ pkgs.nerd-fonts.jetbrains-mono ];
 }
