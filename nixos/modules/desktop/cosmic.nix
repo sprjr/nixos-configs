1571924312904bfaca2dac83a047193e09f5b104
sprjr <patrick@rawlinson.ws>
2026-06-06 20:38:36 -0600

    removing outdated config

--- nixos/modules/desktop/cosmic.nix
+++ nixos/modules/desktop/cosmic.nix
@@ -3,19 +3,7 @@
 {
   imports = [ nixos-cosmic.nixosModules.default ];
 
-  nixpkgs.overlays = [
-    nixos-cosmic.overlays.default
-    (final: prev: {
-      cosmic-edit = prev.cosmic-edit.overrideAttrs (_: {
-        src = final.fetchFromGitHub {
-          owner = "pop-os";
-          repo = "cosmic-edit";
-          rev = prev.cosmic-edit.src.rev;
-          hash = "sha256-GN1Zts+v3ARcrkN+ZkMUSGNOAlIhXSYWRtWAyqUfUrY=";
-        };
-      });
-    })
-  ];
+  nixpkgs.overlays = [ nixos-cosmic.overlays.default ];
 
   nix.settings = {
     substituters = [ "https://cosmic.cachix.org" ];
