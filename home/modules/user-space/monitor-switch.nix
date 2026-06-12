47beffcd49bffa9cbbe63a8d92412d2adb233ee1
sprjr <patrick@rawlinson.ws>
2026-06-12 16:00:34 -0600

    attempting to fix offset monitor positions

--- home/modules/user-space/monitor-switch.nix
+++ home/modules/user-space/monitor-switch.nix
@@ -12,9 +12,9 @@
       text = ''
         #!/usr/bin/env bash
         kscreen-doctor \
-          output.DP-1.enable output.DP-1.mode.2560x1440@165 output.DP-1.position.3191,1080 output.DP-1.scale.1 \
           output.DP-2.enable output.DP-2.mode.3840x2160@60 output.DP-2.position.1920,1080 output.DP-2.scale.1.7 \
-          output.DP-3.enable output.DP-3.mode.3840x2160@60 output.DP-3.position.5751,1080 output.DP-3.scale.1.6 \
+          output.DP-1.enable output.DP-1.mode.2560x1440@165 output.DP-1.position.4179,1080 output.DP-1.scale.1 \
+          output.DP-3.enable output.DP-3.mode.3840x2160@60 output.DP-3.position.6739,1080 output.DP-3.scale.1.6 \
           output.HDMI-A-1.disable
       '';
     };
