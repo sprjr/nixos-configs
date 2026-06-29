c01b32ded54af6ad89021c3b764d36152e6fbfff
sprjr <patrick@rawlinson.ws>
2026-06-29 12:50:30 -0600

    rebuilding to host to integrate with HA

--- nixos/modules/homelab/mosquitto.nix
+++ nixos/modules/homelab/mosquitto.nix
@@ -1,15 +1,23 @@
 { config, pkgs, lib, ... }:
 
 {
+  sops.secrets."mqtt/homeassistant-password" = {
+    owner = "mosquitto";
+    mode = "0400";
+  };
+
   services.mosquitto = {
     enable = true;
     package = pkgs.mosquitto;
     persistence = true;
     listeners = [
       {
-        acl = [ "pattern readwrite #" ];
-	omitPasswordAuth = true; # change later TODO
-	settings.allow_anonymous = true;
+        port = 1883;
+        users.homeassistant = {
+          acl = [ "readwrite #" ];
+          passwordFile = config.sops.secrets."mqtt/homeassistant-password".path;
+        };
+        settings.allow_anonymous = false;
       }
     ];
   };
