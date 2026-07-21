58ee8aa2457e91e98cd57b6056bab0bb4a71a849
sprjr <patrick@rawlinson.ws>
2026-07-21 12:09:50 -0600

    adding

---
+++ home/whale-home.nix
@@ -0,0 +1,25 @@
+{
+  whale-wallpaper,
+  ...
+}:
+
+{
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${whale-wallpaper}";
+      picture-uri-dark = "file://${whale-wallpaper}";
+      picture-options = "zoom";
+    };
+
+    "org/gnome/desktop/screensaver" = {
+      picture-uri = "file://${whale-wallpaper}";
+      picture-options = "zoom";
+    };
+
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  home.stateVersion = "24.11";
+}
