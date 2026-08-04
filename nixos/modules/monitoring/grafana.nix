e33429a2a214b385a6426a5f8889b4f2692c20a1
sprjr <patrick@rawlinson.ws>
2026-08-04 15:05:16 -0600

    one-line fix increasing valid status code acceptance

--- nixos/modules/monitoring/grafana.nix
+++ nixos/modules/monitoring/grafana.nix
@@ -254,7 +254,7 @@ in
             "HTTP/1.1"
             "HTTP/2.0"
           ];
-          valid_status_codes = [ ];
+          valid_status_codes = [ 200 201 204 301 302 401 403 ];
           follow_redirects = false;
           preferred_ip_protocol = "ip4";
           tls_config.insecure_skip_verify = false;
