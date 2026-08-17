9423a1e547095c483c35392800d4ac673d2877c8
sprjr <patrick@rawlinson.ws>
2026-08-16 22:38:05 -0600

    additional attempt to fix auto-unmute

--- nixos/modules/hardware/ca0132-ae5.nix
+++ nixos/modules/hardware/ca0132-ae5.nix
@@ -153,7 +153,7 @@ in
 
         echo "Monitoring ALSA card $card ($dev) for mixer changes"
 
-        stdbuf -oL alsactl monitor "$card" | while read -r line; do
+        stdbuf -oL alsactl monitor "hw:$card" | while read -r line; do
           hda-verb "$dev" 0x02 SET_AMP_GAIN_MUTE 0xb05a 2>/dev/null || true
           hda-verb "$dev" 0x02 SET_AMP_GAIN_MUTE 0xb03c 2>/dev/null || true
         done
