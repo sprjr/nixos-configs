24f116558f5d902b2782cd233491100fde0c5c19
sprjr <patrick@rawlinson.ws>
2026-08-18 12:03:02 -0600

    adding badgey

---
+++ nixos/badgey.nix
@@ -0,0 +1,73 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [ ./modules/system/sops.nix ];
+
+  networking.hostName = "badgey";
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true;
+  };
+  nixpkgs.config.allowUnfree = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  time.timeZone = "America/Denver";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "us";
+
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      amdvlk
+      rocmPackages.clr
+      rocmPackages.clr.icd
+    ];
+  };
+
+  nix = {
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "delete-older-than 21d";
+    };
+  };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 50;
+    priority = 100;
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    wget
+    htop
+    cryptsetup
+    tpm2-tools
+    btrfs-progs
+    compsize
+    rocmPackages.rocminfo
+    rocmPackages.rocm-smi
+  ];
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "monthly";
+    fileSystems = [ "/" ];
+  };
+
+  system.stateVersion = "25.11";
+}
