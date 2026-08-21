0bfe13edf2c26ca4566447ddafc53723eea41f2e
sprjr <patrick@rawlinson.ws>
2026-08-20 23:49:55 -0600

    updating

--- flake.nix
+++ flake.nix
@@ -401,6 +401,7 @@
               ./nixos/modules/homelab/frigate-notify.nix
               ./nixos/modules/virtualisation/podman.nix
               ./nixos/modules/homelab/hermes-agent.nix
+              ./nixos/modules/homelab/frigate-hermes.nix
               (
                 { lib, ... }:
                 lib.mkIf (builtins.pathExists ./nixos/hosts/badgey/facter.json) {
